1e398e9e2330c74e304d333f7d5f3573568ec5b9
Ineza-Agape <i.agape@alustudent.com>
2024-10-04 07:44:03 +0000

    Preparing web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
 # Sets up the servers for deployment of the web_static
 
 
@@ -13,29 +12,11 @@ sudo mkdir -p /data/web_static/shared/
 
 #create fake HTML file
 sudo echo "<html>
-=======
-# Script to set up web servers for deployment of web_static
-
-# Update and install Nginx if not installed
-sudo apt-get update
-sudo apt-get -y install nginx
-
-# Ensure Nginx is running
-sudo service nginx start
-
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file to test the Nginx configuration
-echo "<html>
->>>>>>> 1be626c89d9a90a9e20df9fcd4cb95ea2a36b90a
   <head>
   </head>
   <body>
     Holberton School
   </body>
-<<<<<<< HEAD
 </html>"|sudo tee /data/web_static/releases/test/index.html
 
 # for the symbolic link
@@ -59,21 +40,3 @@ nginx_config="server {
 
 echo "$nginx_config" | sudo tee /etc/nginx/sites-available/default > /dev/null
 sudo service nginx restart
-=======
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a symbolic link, recreate if it exists
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration to serve content
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
-
-# Restart Nginx to apply changes
-sudo service nginx restart
-
-# Exit successfully
-exit 0
->>>>>>> 1be626c89d9a90a9e20df9fcd4cb95ea2a36b90a
