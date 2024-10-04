97f84ca24e1eb4d455b1b12fe469d0f720e92e10
Ineza-Agape <i.agape@alustudent.com>
2024-10-04 09:42:21 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-A Fabric script to distribute
+a Fabric script to distribute
 an archive to web servers using the function do_deploy.
+using 1-pack_web_static.py
 """
 
-from fabric.api import env, run, put, local, sudo
-from os.path import exists
+from fabric.api import env, run, put, local
+from os.path import exists, isdir
 from datetime import datetime
-env.hosts = ['54.160.49.83', '3.90.189.37']  # Add your web server IPs here
+env.hosts = ['54.160.49.83', '3.90.189.37']
 
 
 def do_pack():
@@ -51,88 +52,24 @@ def do_deploy(archive_path):
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        # Upload the archive to the /tmp/ directory on the server
         put(archive_path, '/tmp/')
-
-        # Create the target directory where the archive will be unpacked
         run('mkdir -p {}{}/'.format(path, no_ext))
-
-        # Unpack the archive in the release folder
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-
-        # Remove the archive from /tmp/
         run('rm /tmp/{}'.format(file_n))
-
-        # Move files from web_static to the release folder
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-
-        # Remove the now-empty web_static folder
         run('rm -rf {}{}/web_static'.format(path, no_ext))
-
-        # Delete the old symbolic link (current)
         run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link pointing to the new release
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-
-        # Restart Nginx to apply the changes
-        sudo('service nginx restart')
-
         return True
-    except Exception as e:
-        print(e)
+    except:
         return False
 
 
 def deploy():
     """
-    Creates and distributes an archive to the web servers.
-
-    Returns:
-        bool: True if all steps are successful, False otherwise.
+    creates and distributes an archive to the web servers
     """
     archive_path = do_pack()
     if archive_path is None:
         return False
     return do_deploy(archive_path)
-
-
-def setup_nginx():
-    """
-    Set up Nginx to serve the content of web_static.
-    """
-    try:
-        # Ensure Nginx is installed
-        sudo('apt-get -y update')
-        sudo('apt-get -y install nginx')
-
-        # Create the directory for deployment if it doesn't exist
-        sudo('mkdir -p /data/web_static/releases/test/')
-        sudo('mkdir -p /data/web_static/shared/')
-
-        # Create a test HTML file in the test release folder
-        sudo('echo "Hello AirBnB!" | tee /data/web_static/releases/test/index.html')
-
-        # Create symbolic link
-        sudo('ln -sf /data/web_static/releases/test/ /data/web_static/current')
-
-        # Set proper ownership
-        sudo('chown -R ubuntu:ubuntu /data/')
-
-        # Configure Nginx to serve the web_static content
-        nginx_config = """
-        server {
-            listen 80;
-            server_name your_server_ip;
-
-            location /hbnb_static/ {
-                alias /data/web_static/current/;
-                index index.html;
-            }
-        }
-        """
-        sudo('echo "{}" | tee /etc/nginx/sites-available/default'.format(nginx_config))
-        sudo('service nginx restart')
-    except Exception as e:
-        print(e)
-
