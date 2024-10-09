e1eb03ade5083e7036f82c8acec37de7c7a713cd
Gershom Nsengiyumva <g.nsengiyum@alustudent.com>
2024-10-09 22:53:13 +0400

    fix: document module

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,30 +9,29 @@ This script contains the code for three routes
 """
 
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    # Replace underscores with spaces
     text = text.replace('_', ' ')
     return f"C {text}"
 
-#  there will be a default text for this route
-@app.route("/python/",defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    text = text.replace("_"," ")
+    text = text.replace('_', ' ')
     return f"Python {text}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
