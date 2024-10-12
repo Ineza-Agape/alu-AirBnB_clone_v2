ae80dec2da4269344f9681122af996ad8ecdaa8d
Gershom Nsengiyumva <g.nsengiyum@alustudent.com>
2024-10-12 12:37:55 +0400

    fix: correct the errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,14 @@
-#!/usr/bin/python3
-"""Module that starts a Flask web application"""
+#!/usr/bin/env python3
+"""Module that starts a Flask web application."""
+
+import os
+import sys
+
+# Add the parent directory to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,14 +18,14 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Returns an HTML page with a list of all States in the database"""
-    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    """Return an HTML page with a list of all States in the database."""
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def close_session(exception):
-    """Closes the session"""
+    """Close the session."""
     storage.close()
 
 
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,2 +1,7 @@
 # Web flask project
-Segment in building Airbnb clone project
+
+## Web flask, integration with the frontend
+
+Running the project:
+ * Do the  `pip install` for the imported packages
+ * DO `echo "quit" | HBNB_MYSQL_USER=<> HBNB_MYSQL_PWD=<> HBNB_MYSQL_HOST=<> HBNB_MYSQL_DB=<> HBNB_TYPE_STORAGE=db <file_you_want_to_run>` . Replace `<>` with your original values.
