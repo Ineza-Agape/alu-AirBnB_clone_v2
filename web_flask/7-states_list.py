c27d373423e45e73e81d957ed509cc73f23655e7
Gershom Nsengiyumva <g.nsengiyum@alustudent.com>
2024-10-12 12:52:19 +0400

    imports

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,7 @@
 import os
 import sys
 
+# Add the parent directory to sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
 if parent_dir not in sys.path:
